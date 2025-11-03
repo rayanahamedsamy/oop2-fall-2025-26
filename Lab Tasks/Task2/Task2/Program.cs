5a5537eceedeb3c16d71ef15ac6927d43270f11f
Rayan Ahamed Samy <rayanahamed018@gmail.com>
2025-11-04 00:22:45 +0600

    Delete Lab Tasks directory
    Delete message

--- Lab Tasks/Task2/Task2/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿float ItemPrice = 250.00f;
-float Discount = 15.0f / 100.00f;
-
-float PriceAfterDiscount = ItemPrice - (ItemPrice * Discount);
-Console.WriteLine("The Price After Discount is: " + PriceAfterDiscount + "$");
