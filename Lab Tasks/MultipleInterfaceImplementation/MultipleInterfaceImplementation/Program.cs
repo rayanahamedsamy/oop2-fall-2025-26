a658645242c8ee758bf7b49c4903b56f5aeb8796
NiNET7 <rayanahmedsamy019@gmail.com>
2025-12-30 21:41:07 +0600

    23-52864-2

---
+++ Lab Tasks/MultipleInterfaceImplementation/MultipleInterfaceImplementation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace MultipleInterfaceImplementation;
+
+interface IPrintable
+{
+    void Print();
+}
+
+interface ISerializable
+{
+    void Save();
+}
+
+class Report : IPrintable, ISerializable {
+    public void Print()
+    {
+        Console.WriteLine("Printing The Report!");
+    }
+    public void Save()
+    {
+        Console.WriteLine("Saving The Report!");
+    }
+}
+
+class Program {
+    static void Main()
+    {
+        Report report = new Report();
+        report.Print();
+        report.Save();
+    }
+}
---
+++ Lab Tasks/StudentRecordSystem/StudentRecordSystem/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace StudentRecordSystem;
+
+interface IStudent
+{
+    void Display();
+}
+
+class Student : IStudent
+{
+    private string name;
+    private int[] marks = new int[3];
+
+    public Student(string name)
+    {
+        this.name = name;
+    }
+
+    public int this[int index]
+    {
+        get { return marks[index]; }
+        set { marks[index] = value; }
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Student Name: " + name);
+        for (int i = 0; i < marks.Length; i++)
+        {
+            Console.WriteLine($"Subject " + (i + 1) + ": " + marks[i]);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student stu = new Student("Rayan Ahamed Samy");
+        stu[0] = 85;
+        stu[1] = 90;
+        stu[2] = 78;
+        stu.Display();
+    }
+}
